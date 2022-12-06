0fa69df91d4f49f106730c270f1da3efefd67b69
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-06 22:36:02 +0530

    Chapter 3 Summary

---
+++ class72-Chpter3-Summary-CipherSchools.py
@@ -0,0 +1,29 @@
+# if statement
+name="harshit"
+if name == "harshit":
+    print("you are harshit")
+else:
+    print("you are not harshit")
+
+# while
+i=0
+while i<10:
+    print("hello world")
+    print(i)
+    i += 1
+
+# for Loop
+for i in range(1,11,2):
+    print(i)
+
+# break keyword
+for i in range(1,11):
+    if i==5:
+        break
+    print(i)
+
+# continue keyword
+for i in range(1,11):
+    if i==5:
+        continue
+    print(i)
