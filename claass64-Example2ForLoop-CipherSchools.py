66aca492eb6c3706ce565b35de699236764c4d68
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-06 11:14:09 +0530

    Delete claass64-Example2ForLoop-CipherSchools.py

--- claass64-Example2ForLoop-CipherSchools.py
+++
@@ -1,9 +0,0 @@
-# practice for loop
-# ask user a number like 1256
-# calculate the sum of the digit ---> 1+2+5+6
-
-total=0
-num=input("enter the number : ")
-for i in range(0,len(num)):
-    total+=int(num[i])
-print(total)
