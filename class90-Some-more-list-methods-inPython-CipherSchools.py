000ce824847352f53711a20b4c7d8e04dd307a1f
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-08 22:45:36 +0530

    Some more list methods in Python

---
+++ class90-Some-more-list-methods-inPython-CipherSchools.py
@@ -0,0 +1,21 @@
+# count
+# sort method
+# sorted function
+# reverse
+# clear
+# copy
+fruits=['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+# print(fruits.count('apple'))
+# fruits.sort()
+numbers = [3,5,1,9,10]
+# numbers.sort()
+print(sorted(numbers))
+print(numbers)
+
+# clear
+# numbers.clear()
+# print(numbers)
+
+# copy
+numbers_copy=numbers.copy()
+print(numbers_copy)
