31a27bee92b1fdcfb3a4c961d84460ebdfcac1c9
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-08 20:29:01 +0530

    More methods to add data in Python

---
+++ class87-More-Method-to-Add-data-CipherSchools.py
@@ -0,0 +1,14 @@
+# insert method
+# how to join two list
+# extend method
+
+fruits1 = ['mango', 'orange']
+# fruits1.insert(1,'grapes')
+# print(fruits1)
+fruits2 = ['grapes', 'apple']
+# fruits = fruits1+fruits2
+# print(fruits)
+fruits1.extend(fruits2)
+# fruits1.append(fruits2)
+print(fruits1)
+print(fruits2)
