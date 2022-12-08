fcf6536e4e7b6fb7f5fbc0ecdd231b0ba07991ea
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-08 22:16:47 +0530

    Delete data from list in Python

---
+++ class88-Delete-data-from-list-CipherSchools.py
@@ -0,0 +1,13 @@
+# common methods to delete data from list
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi']
+# pop method
+fruits.pop(1)
+
+#del
+# del fruits[1]
+
+# remove
+
+# fruits.remove('banana')
+
+print(fruits)
