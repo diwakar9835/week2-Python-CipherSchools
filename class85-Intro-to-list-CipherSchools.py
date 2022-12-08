ddd9b2065783dd7a9bb5b4567ae751b4d4cd58d8
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-08 20:08:54 +0530

    Intro to list in Python

---
+++ class85-Intro-to-list-CipherSchools.py
@@ -0,0 +1,17 @@
+# data structures
+# list ---> this chapter
+# ordered collection of items
+
+# you can store any things in list int, float, string 
+
+numbers = [1,2,3,4]
+print(numbers[2])
+
+words = ["word1", "word2" , "word3"]
+print(words[:2])
+
+mixed = [1,2,3,4,5, "five","six",2.3,None]
+print(mixed[-1])
+
+mixed[1] = 'two'
+print(mixed)
