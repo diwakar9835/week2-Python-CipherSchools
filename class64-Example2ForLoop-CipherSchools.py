818be0cb34ddd5974471119b7f95ce4f01c0b7b6
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-10 22:46:14 +0530

    Update class64-Example2ForLoop-CipherSchools.py

--- class64-Example2ForLoop-CipherSchools.py
+++ class64-Example2ForLoop-CipherSchools.py
@@ -2,7 +2,7 @@
 # ask user a number like 1256
 # calculate the sum of the digit ---> 1+2+5+6
 
-total=0
+total=0  
 num=input("enter the number : ")
 for i in range(0,len(num)):
     total+=int(num[i])
