091190364858f13eb8a706147a78fcbc708f9d45
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-10 22:47:53 +0530

    Update class106&107-Chapter5-Exercise5-Solution-CipherSchools.py

--- class106&107-Chapter5-Exercise5-Solution-CipherSchools.py
+++ class106&107-Chapter5-Exercise5-Solution-CipherSchools.py
@@ -1,9 +1,9 @@
 # example
 # input-----> [1,2,5,8], [1,2,7,6]
 # output----> [1,2]
-
+ 
 def common_finder(l1,l2):
-    output=[]
+    output=[]  
     for i in l1:
         if i in l2:
             output.append(i)
